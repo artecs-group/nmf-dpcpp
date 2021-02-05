d5ecb8c7ce3f0fbce04f80517963ac399105c51c
FRYoussef <f.r.youssef@hotmail.com>
2021-02-05 18:41:17 +0100

    changed buffer data into USM

--- src/kernels/common.cpp
+++ src/kernels/common.cpp
@@ -1,9 +1,7 @@
 #include "./common.h"
 
-void adjust_WH(queue &q, buffer<C_REAL, 1> &b_W, buffer<C_REAL, 1> &b_Ht, int N, int M, int K) {
+void adjust_WH(queue q, C_REAL *W, C_REAL *Ht, int N, int M, int K) {
     q.submit([&](handler& cgh) {
-        auto W = b_W.get_access<sycl_read_write>(cgh);
-
         cgh.parallel_for<class check_W>(range<2>(N, K), [=](id <2> ij){
             int i = ij[0];
             int j = ij[1];
@@ -14,8 +12,6 @@ void adjust_WH(queue &q, buffer<C_REAL, 1> &b_W, buffer<C_REAL, 1> &b_Ht, int N,
     });
 	
     q.submit([&](handler& cgh) {
-        auto Ht = b_Ht.get_access<sycl_read_write>(cgh);
-
         cgh.parallel_for<class check_Ht>(range<2>(M, K), [=](id <2> ij){
             int i = ij[0];
             int j = ij[1];
@@ -27,11 +23,8 @@ void adjust_WH(queue &q, buffer<C_REAL, 1> &b_W, buffer<C_REAL, 1> &b_Ht, int N,
 }
 
 
-void V_div_WH(queue &q, buffer<C_REAL, 1> &b_V, buffer<C_REAL, 1> &b_WH, int N, int M) {
+void V_div_WH(queue q, C_REAL *V, C_REAL *WH, int N, int M) {
     q.submit([&](handler& cgh) {
-        auto V = b_V.get_access<sycl_read>(cgh);
-        auto WH = b_WH.get_access<sycl_read_write>(cgh);
-
         cgh.parallel_for<class V_div_WH>(range<2>(N, M), [=](id <2> ij){
             int i = ij[0];
             int j = ij[1];
@@ -42,12 +35,8 @@ void V_div_WH(queue &q, buffer<C_REAL, 1> &b_V, buffer<C_REAL, 1> &b_WH, int N, 
 }
 
 
-void mult_M_div_vect(queue &q, buffer<C_REAL, 1> &b_M, buffer<C_REAL, 1> &b_Maux, buffer<C_REAL, 1> &b_acc, int M, int K) {
+void mult_M_div_vect(queue q, C_REAL *Mat, C_REAL *Maux, C_REAL *acc, int M, int K) {
     q.submit([&](handler& cgh) {
-        auto Mat = b_M.get_access<sycl_read_write>(cgh);
-        auto Maux = b_Maux.get_access<sycl_read>(cgh);
-        auto acc = b_acc.get_access<sycl_read>(cgh);
-
         cgh.parallel_for<class mul_M_div_vect>(range<2>(M, K), [=](id <2> ij){
             int i = ij[0];
             int j = ij[1];
