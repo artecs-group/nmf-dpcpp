01b56cf62ab941372d5c32ca0bbbcf5f72be66d4
FRYoussef <f.r.youssef@hotmail.com>
2021-05-07 12:57:23 +0200

    fixed result error in tree_reduction

--- src/kernels/common.cpp
+++ src/kernels/common.cpp
@@ -1,7 +1,7 @@
 #include "./common.h"
 
 range<3> get_range_in_3d(int data_size, int max_work_group_size) {
-    int dim[3] = {1};
+    int dim[3] = { 1, 1, 1 };
 
     for(int i = 0; i < 3; i++) {
         if(data_size <= max_work_group_size){
@@ -11,7 +11,7 @@ range<3> get_range_in_3d(int data_size, int max_work_group_size) {
         else
             dim[i] = max_work_group_size;
 
-        data_size = ceil(data_size / max_work_group_size);
+        data_size = (data_size + max_work_group_size - 1) / max_work_group_size;
     }
 
     return range<3>(dim[0], dim[1], dim[2]);
@@ -87,21 +87,7 @@ void mult_M_div_vect(queue q, C_REAL *Mat, C_REAL *Maux, C_REAL *acc, int M, int
 
 
 void accum(queue q, C_REAL *acc, C_REAL *X, int N, int M) {
-    int data_size = N*M; // X dim
-    int work_group_size = N; // acc dim
-    int inner_work_groups = M*10; // sub-reduction work groups
-    int inner_work_group_size = (int) ceil(data_size / inner_work_groups);
-    // C_REAL *aux_acc = (C_REAL*) malloc_device<C_REAL>(inner_work_group_size, q);
-
-    // tree_reduction(q, aux_acc, X, data_size, inner_work_group_size);
-    // q.wait();
-
-    // tree_reduction(q, acc, aux_acc, inner_work_group_size, work_group_size);
-    // q.wait();
-
-    // free(aux_acc, q);
-
-    tree_reduction(q, acc, X, data_size, M, work_group_size);
+    tree_reduction(q, acc, X, N, M);
     q.wait();
 
     // q.submit([&](handler& cgh) {
@@ -116,42 +102,38 @@ void accum(queue q, C_REAL *acc, C_REAL *X, int N, int M) {
 }
 
 
-void tree_reduction(queue q, C_REAL *acc, C_REAL *X, int data_size, int work_groups, int work_group_size) {
+void tree_reduction(queue q, C_REAL *acc, C_REAL *X, int N, int M) {
     q.submit([&](auto &h) {
-        sycl::range<3> work_group_range = get_range_in_3d(work_group_size, q.get_device().get_info<cl::sycl::info::device::max_work_group_size>());
-        int work_group_size = work_group_range.get(0) * work_group_range.get(1) * work_group_range.get(2);
-        int fixed_data_size = work_groups * work_group_size;
-        sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::local> scratch(work_group_size, h);
-        // auto sumr = sycl::ONEAPI::reduction(acc, sycl::ONEAPI::plus<>());
+        sycl::range<3> work_group_range = get_range_in_3d(N, q.get_device().get_info<cl::sycl::info::device::max_work_group_size>());
+        int fixed_N = work_group_range.get(0) * work_group_range.get(1) * work_group_range.get(2);
+        std::cout << "N = " << fixed_N << ", M = " << M << std::endl;
+        std::cout << "g0 = " << work_group_range.get(0) << ", g1 = " << work_group_range.get(1) << ", g2 = " << work_group_range.get(2) << std::endl;
+        int data_size = fixed_N * M;
+        sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::local> scratch(fixed_N, h);
 
-        // h.parallel_for(sycl::nd_range{range(fixed_data_size, 1, 1), work_group_range}, sumr, [=](sycl::nd_item<3> item, auto &sumr_arg) {
-        //     int global_id = item.get_global_id(0);
-        //     sumr_arg += X[global_id];
-        // });
-
-        h.parallel_for(sycl::nd_range(range(fixed_data_size, 1, 1), work_group_range), [=](sycl::nd_item<3> item) {
+        h.parallel_for(sycl::nd_range(range(data_size, 1, 1), work_group_range), [=](sycl::nd_item<3> item) {
             int local_i = item.get_local_id(0);
             int local_j = item.get_local_id(1);
             int local_k = item.get_local_id(2);
-
             int local_id = local_i*work_group_range.get(1) + local_j*work_group_range.get(2) + local_k;
-            size_t global_id = item.get_global_id(0);
-            int group_id = item.get_group(0);
 
-            if (global_id < data_size)
+            int group_id = item.get_group(0);
+            size_t global_id = (local_id + group_id) + (local_id * (M-1)); // offset
+
+            if (group_id < data_size)
                 scratch[local_id] = X[global_id];
             else
                 scratch[local_id] = 0;
 
             // Do a tree reduction on items in work-group
-            for (int i = work_group_size / 2; i > 0; i >>= 1) {
+            for (int i = fixed_N / 2; i > 0; i >>= 1) {
                 item.barrier(sycl::access::fence_space::local_space);
 
                 if (local_id < i)
                     scratch[local_id] += scratch[local_id + i];
             }
 
-            if (local_id == 0 && group_id < work_groups)
+            if (local_id == 0 && group_id < M)
                 acc[group_id] = scratch[0];
         });
     });
