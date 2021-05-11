a2f0c83b5b992333bcb122d3a70fd055c4b09e8a
FRYoussef <f.r.youssef@hotmail.com>
2021-05-11 12:26:57 +0200

    simplified some indices

--- src/kernels/common.cpp
+++ src/kernels/common.cpp
@@ -110,13 +110,9 @@ void tree_reduction(queue q, C_REAL *acc, C_REAL *X, int N, int M) {
         sycl::accessor<int, 1, sycl::access::mode::read_write, sycl::access::target::local> scratch(fixed_N, h);
 
         h.parallel_for(sycl::nd_range(range(data_size, 1, 1), work_group_range), [=](sycl::nd_item<3> item) {
-            int local_i = item.get_local_id(0);
-            int local_j = item.get_local_id(1);
-            int local_k = item.get_local_id(2);
-            int local_id = local_i*work_group_range.get(1) + local_j*work_group_range.get(2) + local_k;
-
-            int group_id = item.get_group(0);
-            size_t global_id = (local_id + group_id) + (local_id * (M-1)); // offset
+            int local_id = item.get_local_linear_id();
+            int group_id = item.get_group_linear_id();
+            size_t global_id = local_id * (M-1) + local_id + group_id; // offset
 
             if (global_id < N*M)
                 scratch[local_id] = X[global_id];
