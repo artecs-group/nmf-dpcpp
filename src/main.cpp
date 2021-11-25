a7cf2dd2559b1c63873e2a23a4158c4cf216115b
FRYoussef <f.r.youssef@hotmail.com>
2021-11-25 11:59:41 +0100

    fixed some issues

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ real eps=2.2204e-16;
 double nmf_t{0}, nmf_total{0}, gemm_t{0}, gemm_total{0}, division_t{0}, div_total{0}, red_t{0}, 
 	red_total{0}, mulM_t{0}, mulM_total{0};
 
-void printMATRIX(real **m, int I, int J);
+void printMATRIX(real *m, int I, int J);
 
 
 inline int pow2roundup(int x) {
@@ -66,367 +66,175 @@ double gettime()
 	return(final_time);
 }
 
-real *get_memory1D( int nx )
-{ 
-	int i;
-	real *buffer;	
 
-	if( (buffer=(real *)malloc(nx*sizeof(real)))== NULL )
-	{
-		fprintf( stderr, "ERROR in memory allocation\n" );
-		return( NULL );
-	}
-
-	for( i=0; i<nx; i++ )
-	{
-		buffer[i] = (real)(i*10);
-	}
-
-	return( buffer );
-}
-
-
-void delete_memory1D( real *buffer )
-{ 
-	free(buffer);
-}
-
-unsigned char *get_memory1D_uchar( int nx )
-{ 
-	int i;
-	unsigned char *buffer;	
-
-	if( (buffer=(unsigned char *)malloc(nx*sizeof(int)))== NULL )
-	{
-		fprintf( stderr, "ERROR in memory allocation\n" );
-		return( NULL );
-	}
-
-	for( i=0; i<nx; i++ )
-	{
-		buffer[i] = (int)(0);
-	}
-
-	return( buffer );
-}
-
-
-void delete_memory1D_uchar( unsigned char *buffer )
-{ 
-	free(buffer);
-}
-
-real **get_memory2D( int nx, int ny )
-{ 
-	int i,j;
-	real **buffer;
-	real *tmp_Pinned;
-
-	if( (buffer=(real **)malloc(nx*sizeof(real *)))== NULL )
-	{
-		fprintf( stderr, "ERROR in memory allocation\n" );
-		return( NULL );
-	}
-
-#ifdef pinned_memory
-	cudaError_t status = cudaMallocHost((void**)&(tmp_Pinned), nx*ny*sizeof(real));
-	if (status != cudaSuccess)
-	{
-		fprintf( stderr, "ERROR in pinned memory allocation\n" );
-		free( buffer );
-		return( NULL );
-	}
-	buffer[0] = tmp_Pinned;
-#else
-	if( (buffer[0]=(real *)malloc(nx*ny*sizeof(real)))==NULL )
-	{
-		fprintf( stderr, "ERROR in memory allocation\n" );
-		free( buffer );
-		return( NULL );
-	}
-#endif
-	for( i=1; i<nx; i++ )
-	{
-		buffer[i] = buffer[i-1] + ny;
-	}
-
-	for( i=0; i<nx; i++ )
-		for( j=0; j<ny; j++ )
-		{
-			buffer[i][j] = (real)(i*100+j);
-		}
-
-	return( buffer );
-}
-
-void delete_memory2D( real **buffer )
-{ 
-#ifdef pinned_memory
-	cudaFreeHost(buffer[0]);
-#else
-	free(buffer[0]);
-#endif
-	free(buffer);
-}
-
-real **get_memory2D_renew( int nx, int ny, real *mem )
-{ 
-	int i,j;
-	real **buffer;	
-
-	if( (buffer=(real **)malloc(nx*sizeof(real *)))== NULL )
-	{
-		fprintf( stderr, "ERROR in memory allocation\n" );
-		return( NULL );
-	}
-
-
-	for( i=0; i<nx; i++ )
-	{
-		buffer[i] = &(mem[i*ny]);
-	}
-
-
-	return( buffer );
-}
-
-void matrix_copy1D_uchar( unsigned char *in, unsigned char *out, int nx )
-{ 
-	int i;
-
-	for (i=0; i<nx; i++)
+void matrix_copy1D_uchar(unsigned char *in, unsigned char *out, int nx) {
+	for (int i = 0; i < nx; i++)
 		out[i] = in[i];
 }
 
-void matrix_copy2D(real **in, real **out, int nx, int ny)
-{
-	int i,j;
-	
-	for (i=0; i<nx; i++)
-		for(j=0;j<ny;j++)
-			out[i][j] = in[i][j];
+
+void matrix_copy2D(real *in, real *out, int nx, int ny) {
+	for (int i = 0; i < nx; i++)
+		for(int j = 0; j < ny; j++)
+			out[i*ny + j] = in[i*ny + j];
 }
 
 
-void initWH(real **W, real **Htras, int N, int M, int K, int N_pad, int M_pad)
+void initWH(real *W, real *Htras, int N, int M, int K, int N_pad, int M_pad)
 {
-	int i,j;
-	int ii, jj;
-	
 	int seedi = 0;
-	FILE *fd;
-
-	/* Generated random values between 0.00 - 1.00 */	
+	int size_W = N*K;
+	int size_H = M*K;
+	
+	/* Generated random values between 0.00 - 1.00 */
+	// FILE *fd;
 	// fd = fopen("/dev/urandom", "r");
 	// fread( &seedi, sizeof(int), 1, fd); 
 	// fclose (fd);
 	srand( seedi ); 
 	
-	for (i=0; i<N; i++){
-		for (j=0; j<K; j++)
-			W[i][j] = ((real)(rand()))/RAND_MAX;
-	}
+	for (int i = 0; i < N*K; i++)
+		W[i] = ((real)(rand()))/((real) RAND_MAX);
 
-	for (i=0; i<M; i++){
-        for (j=0; j<K; j++)
-			Htras[i][j] = ((real)(rand()))/RAND_MAX;
-	}
+	for (int i = 0; i < M*K; i++)
+		Htras[i] = ((real)(rand()))/((real) RAND_MAX);
 
 #ifndef RANDOM
 	/* Added to debug */
-	FILE *fIn;
-	real **Wtmp = get_memory2D(N, K);
-	int size_W = N*K;
-	fIn = fopen("w_bin.bin", "r");
-	fread(&Wtmp[0][0], sizeof(real), size_W, fIn);
+	FILE *fIn = fopen("w_bin.bin", "r");;
+	fread(W, sizeof(real), size_W, fIn);
 	fclose(fIn);
-	for (i=0; i<N; i++)
-                for (j=0; j<K; j++)
-			W[i][j] = Wtmp[i][j];
-	delete_memory2D(Wtmp);
 	
-
-	int size_H = M*K;
-	real **Htmp = get_memory2D(M, K);
 	fIn = fopen("h_bin.bin", "r");
-	fread(&Htmp[0][0], sizeof(real), size_H, fIn);
+	fread(H, sizeof(real), size_H, fIn);
 	fclose(fIn);
-	for (i=0; i<M; i++)
-                for (j=0; j<K; j++)
-			Htras[i][j] = Htmp[i][j];
-	delete_memory2D(Htmp);
-	
 #endif
 
 	std::fill(W + (N*K), W + (N_pad*K), 0);
 	std::fill(Htras + (M*K), Htras + (M_pad*K), 0);
 }
 
-void printMATRIX(real **m, int I, int J)
-{
-	int i, j;
-	
+
+void printMATRIX(real *m, int I, int J) {	
 	printf("--------------------- matrix --------------------\n");
 	printf("             ");
-	for (j=0; j<J; j++){
-		if (j<10)
+	for (int j = 0; j < J; j++) {
+		if (j < 10)
 			printf("%i      ", j);
-		else if (j<100)
+		else if (j < 100)
 			printf("%i     ", j);
 		else 
 			printf("%i    ", j);
 	}
 	printf("\n");
 
-	for (i=0; i<I; i++){
+	for (int i = 0; i < I; i++) {
 		if (i<10)
 			printf("Line   %i: ", i);
 		else if (i<100)
 			printf("Line  %i: ", i);
 		else
 			printf("Line %i: ", i);
-		for (j=0; j<J; j++)
-			printf("%5.4f ", m[i][j]);
+
+		for (int j = 0; j < J; j++)
+			printf("%5.4f ", m[i*J + j]);
 		printf("\n");
 	}
 }
 
-void print_WH(real **W, real **Htras, int N, int M, int K)
-{
-	int i,j;	
-	
-	for (i=0; i<N; i++){
+
+void print_WH(real *W, real *Htras, int N, int M, int K) {
+	for (int i = 0; i < N; i++){
 		printf("W[%i]: ", i);
-		for (j=0; j<K; j++)
-			printf("%f ",W[i][j]);
+
+		for (int j = 0; j < K; j++)
+			printf("%f ", W[i*K + j]);
+
 		printf("\n");
 	}
 
-	for (i=0; i<K; i++){
+	for (int i = 0; i < K; i++){
 		printf("H[%i]: ", i);
-        	for (j=0; j<M; j++)
-			printf("%f ",Htras[j][i]);
+
+        	for (int j = 0; j < M; j++)
+				printf("%f ", Htras[j*K + i]);
+
 		printf("\n");
 	}
 }
 
-real **getV(char* file_name, int N, int M)
-{
-	char *data;
-	FILE *fIn;
-	
-	/* Local variables */
-	int i, j;
-	int size_V;
-	real **V;
 
-	fIn = fopen(file_name, "r");
+real *getV(char* file_name, int N, int M)
+{	
+	int size_V = N*M;
+	real *V = new real[size_V];
 
-	V = get_memory2D(N, M);
-	size_V = N*M;
-
-#ifndef RANDOM 
-	if (sizeof(real)==sizeof(float))
-	{
-		fread(&V[0][0], sizeof(float), size_V, fIn);
-		fclose(fIn);
-	} else {
-		int i, j;
-		float *Vaux = (float*)malloc(size_V*sizeof(float));
-		fread(&Vaux[0], sizeof(float), size_V, fIn);
-		fclose(fIn);
-		for (i=0; i<N; i++)
-			for (j=0; j<M; j++)
-				V[i][j] = Vaux[i*M+j];
-	}
+#ifndef RANDOM
+	FILE *fIn = fopen(file_name, "r");
+	fread(V, sizeof(real), size_V, fIn);
+	fclose(fIn);
 #else
 	/* Generated random values between 0.00 - 1.00 */
-	FILE *fd;
 	int seedi = 0;
-        // fd = fopen("/dev/urandom", "r");
-        // fread( &seedi, sizeof(int), 1, fd);
-        // fclose (fd);
-        srand( seedi );
+	// FILE *fd;
+	// fd = fopen("/dev/urandom", "r");
+	// fread( &seedi, sizeof(int), 1, fd);
+	// fclose (fd);
+	srand( seedi );
 
-        for (i=0; i<N; i++)
-			for (j=0; j<M; j++)
-				V[i][j] = ((real)(rand()))/RAND_MAX;
-
-#endif	
+	for (int i = 0; i < size_V; i++)
+		V[i] = ((real)(rand()))/((real) RAND_MAX);
+#endif
 	return(V);
 }
 
 
 /* Gets the difference between matrix_max_index_h and conn_last matrices. */
-int get_difference( unsigned char *classification, unsigned char *last_classification, int nx)
-{
-	int diff;
+int get_difference(unsigned char *classification, unsigned char *last_classification, int nx) {
+	int diff = 0;
 	int conn, conn_last;
-	int i, j;
 	
-	diff = 0;
-	for(i=0; i<nx; i++)
-		for(j=i+1; j<nx; j++) {
+	for(int i = 0; i < nx; i++)
+		for(int j = i+1; j < nx; j++) {
 			conn = (int)( classification[j] == classification[i] );
 			conn_last = (int) ( last_classification[j] == last_classification[i] );
 			diff += ( conn != conn_last );
 		}
 
-	return (diff);
+	return diff;
 }
 
+
 /* Get consensus from the classificacion vector */
-void get_consensus( unsigned char *classification, unsigned char *consensus, int nx )
-{
+void get_consensus(unsigned char *classification, unsigned char *consensus, int nx) {
 	unsigned char conn;
-	int i, j,ii;
+	int ii = 0;
 	
-	ii = 0;
-	for(i=0; i<nx; i++)
-		for(j=i+1; j<nx; j++) {
+	for(int i = 0; i < nx; i++)
+		for(int j = i+1; j < nx; j++) {
 			conn = ( classification[j] == classification[i] );
 			consensus[ii] += conn;
 			ii++;
 		}
 }
 
+
 /* Obtain the classification vector from the Ht matrix */
-void get_classification(real **Htras, unsigned char *classification, int M, int K)
-{
-	int i, j;
-	
+void get_classification(real *Htras, unsigned char *classification, int M, int K) {
 	real max;
 	
-	for (i=0; i<M; i++){
+	for (int i = 0; i < M; i++) {
 		max = 0.0;
-		for (j=0; j<K; j++)
-			if (Htras[i][j] > max)
-			{
+		for (int j = 0; j < K; j++)
+			if (Htras[i*K + j] > max) {
 				classification[i] = (unsigned char)(j);
-				max = Htras[i][j];
+				max = Htras[i*K + j];
 			}
 	}
 }
 
 
-void adjust_WH(real **W, real **Ht, int N, int M, int K)
-{
-	int i, j;
-	
-	for (i=0; i<N; i++)
-		for (j=0; j<K; j++)
-			if (W[i][j]<eps)
-				W[i][j]=eps;
-				
-	for (i=0; i<M; i++)
-		for (j=0; j<K; j++)
-			if (Ht[i][j]<eps)
-				Ht[i][j]=eps;				 
-}
-
-
-real get_Error(real **V, real **W, real **Htras, int N, int M, int K)
-{
+real get_Error(real *V, real *W, real *Htras, int N, int M, int K) {
 	/*
 	* norm( V-WH, 'Frobenius' ) == sqrt( sum( diag( (V-WH)'* (V-WH) ) )
 	* norm( V-WH, 'Frobenius' )**2 == sum( diag( (V-WH)'* (V-WH) ) )
@@ -443,53 +251,45 @@ real get_Error(real **V, real **W, real **Htras, int N, int M, int K)
 	* is equivalent to; error = sum( ( V-Vnew[:,i] .* V-Vnew[:,i] )
 	*
 	*/
-
-
-	real error;
-	int i, j, k;
+	real error = 0.0;
 	real Vnew;
-	
-	error=0.0;
-	for(i=0; i<N; i++)
-	{
-		for(j=0; j<M; j++){
+
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < M; j++){
 			Vnew = 0.0;
-			for(k=0; k<K; k++)
-				Vnew+=W[i][k]*Htras[j][k];
-			error+=(V[i][j]-Vnew)*(V[i][j]-Vnew);
+			for(int k = 0; k < K; k++)
+				Vnew += W[i*K + k] * Htras[j*K + k];
+
+			error += (V[i*M + j] - Vnew) * (V[i*M + j] - Vnew);
 		}
 	}
 	
-	return(error);
-
-
+	return error;
 }
 
 
-void writeSolution(real **W, real **Ht, unsigned char *consensus, int N, int M, int K, int nTests)
+void writeSolution(real *W, real* Ht, unsigned char *consensus, int N, int M,
+    int K, int nTests)
 {
-	int i, j;
 	FILE *fOut;
 	char file[100];
-	real **H;
+	real *H = new real[K*M];
 	
-	H = get_memory2D(K, M);
-	for (i=0; i<K; i++)
-		for (j=0; j<M; j++)
-			H[i][j] = Ht[j][i];
+	for (int i = 0; i < K; i++)
+		for (int j = 0; j < M; j++)
+			H[i*M + j] = Ht[j*K + i];
 	
 	sprintf(file,"solution-NMFLeeSeung_%i", K);
 	fOut = fopen(file, "w");
 	fwrite( &N, sizeof(int), 1, fOut);
 	fwrite( &M, sizeof(int), 1, fOut);
 	fwrite( &K, sizeof(int), 1, fOut);
-	fwrite( W[0], sizeof(real), N*K, fOut);
-	fwrite( H[0], sizeof(real), K*M, fOut);
+	fwrite( W, sizeof(real), N*K, fOut);
+	fwrite( H, sizeof(real), K*M, fOut);
 	fwrite( &nTests, sizeof(int), 1, fOut);
 	fwrite( consensus, sizeof(unsigned char), (M*(M-1))/2, fOut);
 	fclose( fOut );
-	
-	delete_memory2D(H);
+	delete [] H;
 }
 
 
@@ -497,22 +297,13 @@ void nmf(int niter, real *d_V, real *d_WH, real *d_W, real *d_Htras, real *d_Wau
 	real *d_accW, real *d_accH,
 	int N, int M, int K, int N_pad, int M_pad)
 {
-	int iter;
-	
-	int i, j, k;
-
-	real diff, tot_diff;
-	real Vn;
-
-	double t0, t1;
-
 	/*************************************/
 	/*                                   */
 	/*      Main Iterative Process       */
 	/*                                   */
 	/*************************************/
 	nmf_t = gettime();
-	for (iter=0; iter<niter; iter++)
+	for (int iter = 0; iter < niter; iter++)
 	{
 		/*******************************************/
 		/*** H = H .* (W'*(V./(W*H))) ./ accum_W ***/
@@ -573,15 +364,15 @@ int main(int argc, char *argv[])
 	int i,j;
 	real obj;
 
-	real **V;
-	real **W, **W_best;
-	real **Htras, **Htras_best;
+	real *V;
+	real *W, *W_best;
+	real *Htras, *Htras_best;
 	unsigned char *classification, *last_classification;
 	unsigned char *consensus; /* upper half-matrix size M*(M-1)/2 */
 
 	/* Auxiliares para el computo */
-	real **WH;
-	real **Haux,  **Waux;
+	real *WH;
+	real *Haux, *Waux;
 	real *acumm_W;
 	real *acumm_H;
 	
@@ -608,56 +399,48 @@ int main(int argc, char *argv[])
 	
 	real error;
 	real error_old=9.99e+50;
-
-        setbuf( stdout, NULL );
+    setbuf( stdout, NULL );
 	
-	if (argc==7){
-		strcpy(file_name, argv[1]);
-		N              = atoi(argv[2]);
-		N_pad      	   = pow2roundup(N);
-		M              = atoi(argv[3]);
-		M_pad          = pow2roundup(M);
-		K              = atoi(argv[4]);
-		nTests         = atoi(argv[5]);
-		stop_threshold = atoi(argv[6]);
-	} else {
+	if(argc != 7) {
 	 	printf("./exec dataInput.bin N M K nTests stop_threshold (argc=%i %i)\n", argc, atoi(argv[2]));
-
 		return(0);
 	}
 
-	V = getV(file_name, N, M_pad);
-	printf("file=%s\nN=%i M=%i K=%i nTests=%i stop_threshold=%i\n", file_name, N, M, K, nTests, stop_threshold);	
+	strcpy(file_name, argv[1]);
+	N              = atoi(argv[2]);
+	N_pad      	   = pow2roundup(N);
+	M              = atoi(argv[3]);
+	M_pad          = pow2roundup(M);
+	K              = atoi(argv[4]);
+	nTests         = atoi(argv[5]);
+	stop_threshold = atoi(argv[6]);
 
-	W          = get_memory2D(N_pad, K);
-	Htras      = get_memory2D(M_pad, K);
-	W_best     = get_memory2D(N, K);
-	Htras_best = get_memory2D(M, K);
-	
-	WH         = get_memory2D(N, M_pad);
-	
-	Haux       = get_memory2D(M, K);
-	Waux       = get_memory2D(N, K);
-	
-	acumm_W    = get_memory1D(K);
-	acumm_H    = get_memory1D(K);
-	
-	classification      = get_memory1D_uchar(M);
-	last_classification = get_memory1D_uchar(M);
-	consensus           = get_memory1D_uchar(M*(M-1)/2);
+	printf("file=%s\nN=%i M=%i K=%i nTests=%i stop_threshold=%i\n", file_name, N, M, K, nTests, stop_threshold);
+
+	V          = getV(file_name, N, M_pad);
+	W          = new real[N_pad*K];
+	Htras      = new real[M_pad*K];
+	W_best     = new real[N*K];
+	Htras_best = new real[M*K];
+	WH         = new real[N*M_pad];
+	Haux       = new real[M*K];
+	Waux       = new real[N*K];
+	acumm_W    = new real[K];
+	acumm_H    = new real[K];
+	classification      = new unsigned char[M];
+	last_classification = new unsigned char[M];
+	consensus           = new unsigned char[M*(M-1)/2];
 
 	cublasInit();
 	cudaMalloc((void **)&d_V,      N*M_pad*sizeof(real));
-	cudaMemcpy(d_V, V[0], N*M_pad*sizeof(real), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_V, V,            N*M_pad*sizeof(real), cudaMemcpyHostToDevice);
 	cudaMalloc((void **)&d_W,      N_pad*K*sizeof(real));
 	cudaMalloc((void **)&d_Htras,  M_pad*K*sizeof(real));
 	cudaMalloc((void **)&d_WH,     N*M_pad*sizeof(real));
-
 	cudaMalloc((void **)&d_Waux,   N*K*sizeof(real));
 	cudaMalloc((void **)&d_Haux,   M*K*sizeof(real));
-
-	cudaMalloc((void **)&d_acumm_W,K*sizeof(real));
-	cudaMalloc((void **)&d_acumm_H,K*sizeof(real));
+	cudaMalloc((void **)&d_acumm_W, K*sizeof(real));
+	cudaMalloc((void **)&d_acumm_H, K*sizeof(real));
 
 	/**********************************/
 	/******     MAIN PROGRAM     ******/
@@ -670,8 +453,8 @@ int main(int argc, char *argv[])
 		initWH(W, Htras, N, M, K, N_pad, M_pad);
 
 		timeGPU0 = gettime();
-		cudaMemcpy(d_W,     W[0],     N_pad*K*sizeof(real), cudaMemcpyHostToDevice);
-		cudaMemcpy(d_Htras, Htras[0], K*M_pad*sizeof(real), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_W, W, N_pad * K * sizeof(real), cudaMemcpyHostToDevice);
+		cudaMemcpy(d_Htras, Htras, K * M_pad * sizeof(real), cudaMemcpyHostToDevice);
 		timeGPU1 = gettime();
 		timeGPU2CPU += timeGPU1-timeGPU0;
 
@@ -693,8 +476,8 @@ int main(int argc, char *argv[])
 			adjust_WH_GPU(d_W, d_Htras, N, M, K);
 
 			timeGPU0 = gettime();
-			cudaMemcpy(W[0],     d_W,     N_pad*K*sizeof(real), cudaMemcpyDeviceToHost);
-			cudaMemcpy(Htras[0], d_Htras, K*M_pad*sizeof(real), cudaMemcpyDeviceToHost);
+			cudaMemcpy(W, d_W, N_pad * K * sizeof(real), cudaMemcpyDeviceToHost);
+			cudaMemcpy(Htras, d_Htras, K * M_pad * sizeof(real), cudaMemcpyDeviceToHost);
 			timeGPU1 = gettime();
 			timeGPU2CPU += timeGPU1-timeGPU0;
 
@@ -749,17 +532,16 @@ int main(int argc, char *argv[])
 	writeSolution(W_best, Htras_best, consensus, N, M, K, nTests);
 
 	/* Free memory used */
-	delete_memory2D(W);
-	delete_memory2D(Htras);
-	delete_memory2D(W_best);
-	delete_memory2D(Htras_best);	
-	delete_memory2D(WH);
-	delete_memory2D(Waux);
-	delete_memory2D(Haux);
-	delete_memory1D(acumm_W);
-	delete_memory1D(acumm_H);
-	delete_memory1D_uchar(classification);
-	delete_memory1D_uchar(last_classification);
-	delete_memory1D_uchar(consensus);
-
+	delete [] W;
+	delete [] Htras;
+	delete [] W_best;
+	delete [] Htras_best;	
+	delete [] WH;
+	delete [] Waux;
+	delete [] Haux;
+	delete [] acumm_W;
+	delete [] acumm_H;
+	delete [] classification;
+	delete [] last_classification;
+	delete [] consensus;
 }
